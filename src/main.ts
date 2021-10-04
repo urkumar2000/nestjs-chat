ec9c79c05da8ec22d8fc327b7ec205f8d43319fd
urkumar2000 <50820005+urkumar2000@users.noreply.github.com>
2021-10-04 18:09:01 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    credentials: true,
-    origin: 'http://localhost:4200',
-  });
+//   app.enableCors({
+//     credentials: true,
+//     origin: 'http://localhost:4200',
+//   });
 
   await app.listen(3000);
 }
